37313170f55315d5dc9749ea01023c130edacf90
Lucila Acerbi <lucilaacerbi21@gmail.com>
2022-10-02 21:58:31 -0300

    se unen eventos

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+// Evento enviar formulario 
+
+// const boton = document.getElementById ('btnEnviar');
+
+// boton.addEventListener ('submit', (event) => {
+//     event.preventDefault();
+//     let userName = document.querySelector('#userName').value;
+//     let email = document.querySelector ('#email').value;
+//     let mensage = document.querySelector('#mensage').value;
+    
+//     console.log(userName, email, mensage);
+// });
+
+
+// Evento mostrar clicks en icono instagram
+
+const click = document.getElementById ('whatsApp');
+
+click.addEventListener ('click', (event) => {
+    event.preventDefault();
+    console.log ('se hizo click');
+})
