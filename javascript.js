d9af06058dd905e2e709463fedbb41677af893dd
Lucila Acerbi <lucilaacerbi21@gmail.com>
2022-10-13 18:35:49 -0300

    se incluye libreria

--- javascript.js
+++ javascript.js
@@ -2,33 +2,32 @@
 
 const boton = document.getElementById ('btnEnviar');
 
-boton.addEventListener ('submit', (e) => {
-    e.preventDefault();
-    let userName = document.getElementById('userName').value;
-    let email = document.getElementById('email').value;
-    let mensage = document.getElementById('mensage').value;
+// boton.addEventListener ('submit', (e) => {
+//     e.preventDefault();
+//     let userName = document.getElementById('userName').value;
+//     let email = document.getElementById('email').value;
+//     let mensage = document.getElementById('mensage').value;
     
-    console.log(userName, email, mensage);
+//     console.log(userName, email, mensage);
 
-    localStorage.setItem ('Nombre', userName);
-    localStorage.setItem ('correo', email);
-    localStorage.setItem ('mensaje', mensage);
-});
+//     localStorage.setItem ('Nombre', userName);
+//     localStorage.setItem ('correo', email);
+//     localStorage.setItem ('mensaje', mensage);
+// });
 
 // Evento mostrar clicks en boton enviar
 
-const click = document.getElementById ('btnEnviar');
+// const click = document.getElementById ('btnEnviar');
 
-click.addEventListener ('click', (e) => {
-    e.preventDefault();
-    console.log ('se hizo click');
-})
+// click.addEventListener ('click', (e) => {
+//     e.preventDefault();
+//     console.log ('se hizo click');
+// })
 
 // Almacenarmiento en LocalStorage, simulo consulta en formulario
 
 const usuario = localStorage.setItem ('Nombre', 'Lucila')
 const mail = localStorage.setItem ('correo', 'lucila@mail.com')
 const mensaje = localStorage.setItem ('mensaje', 'Hola, quisiera hacer una consulta')
-
 
 
---
+++ main.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById ('btnEnviar');
+
+btn.addEventListener ('click', () => {
+    Swal.fire({
+        icon: 'success',
+        title: '<b>Mensaje enviado<b>',
+        text: 'Revisa tu casilla, te contactaremos',
+        showConfirmButton: false, 
+        timer: 1500
+    });
+})
