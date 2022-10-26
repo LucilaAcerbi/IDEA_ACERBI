085467572e28e7fd7e354df3fa08b2e19eaf58a2
Lucila Acerbi <lucilaacerbi21@gmail.com>
2022-10-25 21:29:17 -0300

    se configura email en form

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
-const btn = document.getElementById ('btnEnviar');
+const form = document.getElementById ('form');
+const user = document.getElementById ('user_name');
+const email = document.getElementById ('e-mail');
+const message = document.getElementById ('message');
 
-btn.addEventListener ('click', () => {
-    Swal.fire({
-        icon: 'success',
-        title: '<b>Mensaje enviado<b>',
-        text: 'Revisa tu casilla, te contactaremos',
-        showConfirmButton: false, 
-        timer: 1500
-    });
+const sendEmail = async (body) => {
+    const settings = {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify(body),
+    }
+    const response =  await fetch ('https://api.emailjs.com/api/v1.0/email/send', settings);
+    const data = await response.json();
+    return data;
+};
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const body = {
+        service_id: 'service_wdjqzjk',
+        template_id: 'template_pipgmep',
+        user_id: '10jZEFdyS7hUcX0pA',
+        template_params: {
+            'to_name': user.value,
+            'from_name': email.value,
+            'message': message.value
+        }
+    };
+
+    sendEmail(body)
 })
+
+        
+// btn.addEventListener ('click', () => {
+//     Swal.fire({
+//         icon: 'success',
+//         title: '<b>Mensaje enviado<b>',
+//         text: 'Revisa tu casilla, te contactaremos',
+//         showConfirmButton: false, 
+//         timer: 1500
+//     });
+// })
 
